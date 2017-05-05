93e5fd88ad1c99878f9ee78de5739da8cb6f697a
Velliz <diditvelliz@gmail.com>
2017-05-05 17:41:26 +0700

    permission query

--- model/Permissions.php
+++ model/Permissions.php
@@ -18,21 +18,22 @@ class Permissions
 
     public static function GetAll()
     {
-        return DBI::Prepare("SELECT * FROM `permissions` WHERE (dflag = 0)")->GetData();
+        return DBI::Prepare("SELECT * FROM permissions WHERE (dflag = 0)")->GetData();
     }
 
     public static function GetID($id)
     {
-        return DBI::Prepare("SELECT * FROM `permissions` WHERE (`id` = @1) AND (dflag = 0);")->GetData($id);
+        return DBI::Prepare("SELECT * FROM permissions WHERE (`id` = @1) AND (dflag = 0);")->GetData($id);
     }
 
     public static function GetByToken($token)
     {
-        return DBI::Prepare("SELECT * FROM `permissions` WHERE (`token` = @1) AND (dflag = 0);")->GetData($token);
+        return DBI::Prepare("SELECT * FROM permissions WHERE (`token` = @1) AND (dflag = 0);")->GetData($token);
     }
 
     public static function GetByApp($appId)
     {
-        return DBI::Prepare("SELECT * FROM `permissions` WHERE (`dflag` = 0) AND (appid = @1);")->GetData($appId);
+        return DBI::Prepare("SELECT id, created, pname, pcode, description FROM permissions 
+          WHERE (`dflag` = 0) AND (appid = @1);")->GetData($appId);
     }
 }
