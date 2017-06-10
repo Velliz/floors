189246886e6b54c32aa29e08304296957dc9127f
Velliz <diditvelliz@outlook.com>
2017-06-10 23:28:06 +0700

    openshift v3 update 4

--- config/database.php
+++ config/database.php
@@ -1,7 +1,7 @@
 <?php
 return array(
     'dbType' => 'mysql',
-    'host' => 'localhost',
+    'host' => '172.30.61.161',
     'user' => 'user5M8',
     'pass' => 'pl5lWnRUMQmLUtFw',
     'dbName' => 'sampledb',
