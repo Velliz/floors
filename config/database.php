b882a94c7ee91ca06abf4121c766eaa39a51f22d
Velliz <diditvelliz@outlook.com>
2017-06-10 22:21:16 +0700

    openshift v3

--- config/database.php
+++ config/database.php
@@ -1,9 +1,9 @@
 <?php
 return array(
     'dbType' => 'mysql',
-    'host' => 'localhost',
-    'user' => 'root',
-    'pass' => '',
-    'dbName' => 'floors',
+    'host' => '172.30.61.161',
+    'user' => 'user5M8',
+    'pass' => 'pl5lWnRUMQmLUtFw',
+    'dbName' => 'sampledb',
     'port' => 3306
 );
